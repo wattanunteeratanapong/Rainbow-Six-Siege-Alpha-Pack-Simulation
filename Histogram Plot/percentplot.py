d4871c54e396f4dd6e1cbed9995cbf4c8d126f13
Ikkyu <126569245+wattanunteeratanapong@users.noreply.github.com>
2024-06-07 15:00:37 +0700

    Add files via upload

---
+++ Histogram Plot/percentplot.py
@@ -0,0 +1,48 @@
+import random
+import matplotlib.pyplot as plt
+
+match_list = []
+percent_data = []
+
+
+# Random Method
+# Input : Current Alpha Pack Chance
+# Output : True(Drop Alpha Pack) or False(Not Drop Alpha Pack)
+def chance(percent):
+    random_number = random.random()
+    return random_number < (percent / 100)
+
+
+# Simulation Method
+def alphapack():
+    match_list = [random.randint(0, 1) for _ in range(40)]
+    percent = 0
+    matchs = 0
+    for match in match_list:
+        if match == 1:
+            percent = percent + 2.5
+            matchs = matchs + 1
+            if chance(percent):
+                percent_data.append(percent)
+                return
+        elif match == 0:
+            percent = percent + 2.5
+            matchs = matchs + 1
+
+
+# Simulation
+def simulation(generate_times):
+    for i in range(generate_times):
+        alphapack()
+        
+simulation(100000)
+
+
+# Plot Histogram
+plt.hist(percent_data, bins=range(1, 100), edgecolor='black', density=False) 
+plt.xticks(range(1, 100)) 
+plt.xlabel('Percent Chance')
+plt.ylabel('Frequency')
+plt.title('Distribution of Percent Chance Increments to Obtain Alpha Pack')
+plt.grid(True)
+plt.show()

622f00199cfaa0d9359dfb8f52a66c86d141238a
Ikkyu <126569245+wattanunteeratanapong@users.noreply.github.com>
2024-06-11 23:54:23 +0700

    Add files via upload

---
+++ Ranked/Histogram Plot/matchplot.py
@@ -0,0 +1,49 @@
+import random
+import matplotlib.pyplot as plt
+
+match_list = []
+match_data = []
+
+
+# Random Method
+# Input : Current Alpha Pack Percent Chance
+# Output : True(Drop Alpha Pack) or False(Not Drop Alpha Pack)
+def chance(percent):
+    random_number = random.random()
+    return random_number < (percent / 100)
+
+
+# Simulation Method
+def alphapack():
+    match_list = [random.randint(0, 1) for i in range(40)]
+    percent = 0
+    matchs = 0
+    for match in match_list:
+        if match == 1:
+            percent = percent + 3
+            matchs = matchs + 1
+            if chance(percent):
+                match_data.append(matchs)
+                return
+        elif match == 0:
+            percent = percent + 2.5
+            matchs = matchs + 1
+
+
+# Simulation
+def simulation(generate_times):
+    for i in range(generate_times):
+        alphapack()
+        
+simulation(100000)
+# You can also change the number of simulations to 1,000,000 to get a more accurate histogram.
+
+
+# Plot Histogram
+plt.hist(match_data, bins=range(1, 40), edgecolor='black', density=False) 
+plt.xticks(range(1, 40)) 
+plt.xlabel('Number of Matches')
+plt.ylabel('Frequency')
+plt.title('Distribution of Matches to Obtain Alpha Pack (Ranked)')
+plt.grid(True)
+plt.show()
